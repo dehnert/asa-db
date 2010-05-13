4210a1be09bb770eb7008d4ad93a859f0b665c3b
Alex Dehnert <adehnert@mit.edu>
2010-05-13 06:23:28 -0400

    Why is everything so long...?

--- asadb/groups/models.py
+++ asadb/groups/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Group(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     abbreviation = models.CharField(max_length=10, blank=True)
     description = models.TextField()
     activity_category = models.ForeignKey('ActivityCategory', null=True, blank=True, )
     website_url = models.URLField()
     constitution_url = models.URLField()
     meeting_times = models.TextField(blank=True)
-    advisor_name = models.CharField(max_length=50, blank=True)
+    advisor_name = models.CharField(max_length=100, blank=True)
     num_undergrads = models.IntegerField(null=True, blank=True, )
     num_grads = models.IntegerField(null=True, blank=True, )
     num_community = models.IntegerField(null=True, blank=True, )
@@ -18,7 +18,7 @@ class Group(models.Model):
     officer_email = models.EmailField()
     main_account_id = models.IntegerField(null=True, blank=True, )
     funding_account_id = models.IntegerField(null=True, blank=True, )
-    athena_locker = models.CharField(max_length=14)
+    athena_locker = models.CharField(max_length=20)
     recognition_date = models.DateField()
     update_date = models.DateTimeField()
     updater = models.CharField(max_length=30) # match Django username field
