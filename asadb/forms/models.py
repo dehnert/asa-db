80194d89e37b8a5224493783e1fcea3a4318adde
Alex Dehnert <adehnert@mit.edu>
2010-05-14 02:49:53 -0400

    Add legends

--- asadb/forms/models.py
+++ asadb/forms/models.py
@@ -9,9 +9,9 @@ class FYSM(models.Model):
     display_name = models.CharField(max_length=50)
     year = models.IntegerField()
     website = models.URLField()
-    join_url = models.URLField()
-    contact_email = models.EmailField()
-    description = models.TextField()
+    join_url = models.URLField(help_text="If you have a specific web page for students interested in joining your group, you can link to it here. If you leave this blank it will default to your website.")
+    contact_email = models.EmailField(help_text="Give an address for students interested in joining the group to email (e.g., an officers list)")
+    description = models.TextField(help_text="Explain in about three or four sentences what your group does and why incoming freshmen should get involved.")
     logo = models.ImageField(upload_to='fysm/logos', )
     tags = models.ManyToManyField('FYSMTag', blank=True, )
 
