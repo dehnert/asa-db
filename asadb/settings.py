062a126ca2ffad262e7b04b8b7a9815ac118d377
Alex Dehnert <adehnert@mit.edu>
2011-05-21 02:30:59 -0400

    Add django-form-utils as a component app

--- asadb/settings.py
+++ asadb/settings.py
@@ -99,6 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'form_utils',
     'reversion',
     'south',
     'groups',
