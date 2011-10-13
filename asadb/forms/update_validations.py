654c9afef58bf60b6dcd6092d0b0646560309d68
Alex Dehnert <adehnert@mit.edu>
2011-10-13 03:55:09 -0400

    Add script to redo student status validations
    This is useful if you forget to run groups/load_people.py before running the
    migration that adds the "valid" column to the PersonMembershipUpdate model.

---
+++ asadb/forms/update_validations.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+if __name__ == '__main__':
+    cur_file = os.path.abspath(__file__)
+    django_dir = os.path.abspath(os.path.join(os.path.dirname(cur_file), '..'))
+    proj_dir = os.path.abspath(os.path.join(django_dir, '..'))
+    sys.path.append(django_dir)
+    sys.path.append(proj_dir)
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import forms.models
+import groups.models
+
+
+def revalidate_confirmations():
+    confirmations = forms.models.PersonMembershipUpdate.objects.filter(deleted__isnull=True, valid=forms.models.VALID_UNSET)
+    accounts = groups.models.AthenaMoiraAccount
+    for confirmation in confirmations:
+        try:
+            person = accounts.active_accounts.get(username=confirmation.username)
+            if person.is_student():
+                confirmation.valid = forms.models.VALID_AUTOVALIDATED
+            else:
+                confirmation.valid = forms.models.VALID_AUTOREJECTED
+
+        except accounts.DoesNotExist:
+            print "Could not find %s; rejecting" % (confirmation.username, )
+            confirmation.valid = forms.models.VALID_AUTOREJECTED
+        confirmation.save()
+
+if __name__ == '__main__':
+    revalidate_confirmations()
