5543d51f6e3264f4d369ec2a6e47ee975a8a550b
Alex Dehnert <adehnert@mit.edu>
2011-12-18 01:00:30 -0500

    Moira->Django sync script
    Major changes from 776830deb5:
    * Actually sync
    * Use AFS

--- asadb/util/sync_moira_authz.py
+++ asadb/util/sync_moira_authz.py
@@ -1,17 +1,88 @@
 #!/usr/bin/python
 
+import afs.pts
 import ldap
 import ldap.dn
 import ldap.filter
 import os
 import sys
 
-class LDAPConnector(object):
+if __name__ == '__main__':
+    cur_file = os.path.abspath(__file__)
+    django_dir = os.path.abspath(os.path.join(os.path.dirname(cur_file), '..'))
+    sys.path.append(django_dir)
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django.contrib.auth.models
+
+import mit
+
+class DjangoConnector(object):
     def __init__(self, ):
+        self.dj_groups = django.contrib.auth.models.Group.objects
+
+    def sync_members(self, sys_name, dj_name, ):
+        kept = []
+        added = []
+        nonexist = []
+        created = []
+        removed = []
+        sys_members = self.get_members(sys_name)
+        dj_group = self.dj_groups.get(name=dj_name)
+        dj_members = dj_group.user_set.all()
+        dj_usernames = set([m.username for m in dj_members])
+        for username in sys_members:
+            if username in dj_usernames:
+                kept.append(username)
+            else:
+                # Need to add to the Django group
+                try:
+                    user, is_new = mit.get_or_create_mit_user(username, )
+                    if is_new: created.append(username)
+                    user.groups.add(dj_group)
+                    added.append(username)
+                except ValueError:
+                    nonexist.append(username)
+        for user in dj_members:
+            username = user.username
+            if username in sys_members:
+                assert username in kept
+            else:
+                user.groups.remove(dj_group)
+                removed.append(username)
+        return {
+            'change' : len(added) + len(removed),
+            'keep' : kept,
+            'add'  : added,
+            'create' : created,
+            'nonexist' : nonexist,
+            'remove': removed,
+        }
+
+    def sync_many(con, what, force_print=False, ):
+        changed = False
+        results = {}
+        for sys_name, dj_group in what:
+            assert dj_group not in results
+            results[dj_group] = con_afs.sync_members(sys_name, dj_group)
+            if results[dj_group]['change']: changed = True
+        if changed or force_print:
+            for group in results:
+                print ""
+                print "Results for %s:" % (group, )
+                for key, value in results[group].items():
+                    print "%7s:\t%s" % (key, value, )
+
+
+class LDAPConnector(DjangoConnector):
+    name = "LDAP"
+
+    def __init__(self, *args, **kwargs):
+        super(LDAPConnector, self).__init__(*args, **kwargs)
         self.con = ldap.initialize('ldaps://ldap-too.mit.edu')
         self.con.simple_bind_s("", "")
 
-    def get_members_ldap(self, groupname):
+    def get_members(self, groupname):
         base_dn = 'ou=lists,ou=moira,dc=mit,dc=edu'
         groupfilter = ldap.filter.filter_format('(&(objectClass=group)(displayName=%s))', [groupname])
         result = self.con.search_s(base_dn, ldap.SCOPE_SUBTREE, groupfilter, )
@@ -34,10 +105,32 @@ class LDAPConnector(object):
                 ret.add(('kerberos', c_val_val, ))
             else:
                 assert False, "Don't know what %s is" % (c_type, )
-        return ret
+        return [r[1] for r in ret if r[0] == 'user']
+
+class AFSConnector(DjangoConnector):
+    name = "AFS"
+
+    def __init__(self, *args, **kwargs):
+        super(AFSConnector, self).__init__(*args, **kwargs)
+        # TODO: possibly kinit and aklog
+        self.pts = afs.pts.PTS(sec=afs.pts.PTS_ENCRYPT, cell='athena.mit.edu', )
+    def get_members(self, groupname, ):
+        afs_members = self.pts.getEntry("system:%s" % (groupname, )).members
+        members = [ m.name for m in afs_members ]
+        return members
+
+sync_pairs = [
+    ('asa-internal', 'asa-ebm', ),
+]
+
+def test_memberships(cons):
+    for sys_name, dj_group in sync_pairs:
+        for con in cons:
+            members = con.get_members(sys_name)
+            print "%s\t%s\t%s" % (con.name, sys_name, sorted(members))
 
 if __name__ == '__main__':
-    con = LDAPConnector()
-    for listname in sys.argv[1:]:
-        members = con.get_members_ldap(listname)
-        print "%s\t%s" % (listname, members)
+    con_afs = AFSConnector()
+    #con_ldap = LDAPConnector()
+    #test_memberships([con_afs, con_ldap, ])
+    con_afs.sync_many(sync_pairs)
