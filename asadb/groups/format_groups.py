b90b977c3ed521bf287f5e7ddb5899601a5bbb6d
Alex Dehnert <adehnert@mit.edu>
2012-03-23 01:28:26 -0400

    Fixes to groups/format_groups.py to make it work

--- asadb/groups/format_groups.py
+++ asadb/groups/format_groups.py
@@ -15,30 +15,30 @@ from django.db.models import Q
 import groups.models
 
 
-Qsemiactive = Q(group_status__slug__in=('active', 'suspended', ))
+Qsa = Q(group_status__slug__in=('active', 'suspended', ))
 functions = {
     'finboard' : {
-        'format' : "%(STUDENT_GROUP_NAME)s;%(OFFICER_EMAIL)s",
-        'predicate' : Q(group_funding__slug='undergraduate', group_class__slug='mit-funded', ),
+        'format' : "%(name)s;%(officer_email)s",
+        'predicate' : Qsa & Q(group_funding__slug='undergrad', group_class__slug='mit-funded', ),
     },
     'nolist' : {
-        'format' : "%(STUDENT_GROUP_NAME)s <%(OFFICER_EMAIL)s>",
-        'predicate' : ~Q(officer_email=""),
+        'format' : "%(name)s <%(officer_email)s>",
+        'predicate' : Qsa & Q(officer_email=""),
     },
     'asa-official' : {
         'format' : '"%(name)s" <%(officer_email)s>',
         'predicate' : Q(group_status__slug='active'),
     },
     'emails-only' : {
-        'format' : '%(OFFICER_EMAIL)s',
-        'predicate' : Qsemiactive,
+        'format' : '%(officer_email)s',
+        'predicate' : Qsa,
     },
 }
 
 def do_output(mode):
     format = functions[mode]['format']
     predicate = functions[mode]['predicate']
-    gs = groups.models.Group.objects.filter(Qsemiactive & predicate)
+    gs = groups.models.Group.objects.filter(predicate)
     for group in gs:
         print format % {
             'name':group.name,
