0229bd52f0ec8656e3cdcd1ff003c1f58b8625f5
Alex Dehnert <adehnert@mit.edu>
2012-07-30 01:46:18 -0400

    Filter by GroupConfirmationCycle

--- asadb/forms/admin.py
+++ asadb/forms/admin.py
@@ -59,7 +59,7 @@ class Admin_PersonMembershipUpdate(admin.ModelAdmin):
         'deleted',
         'valid',
     )
-    list_filter = ('valid', 'groups', )
+    list_filter = ('valid', 'cycle', 'groups', )
     list_display_links = ('pk', 'username', 'cycle', )
     search_fields = ('username', 'groups__officer_email', 'groups__name', 'groups__abbreviation', )
 admin.site.register(forms.models.PersonMembershipUpdate, Admin_PersonMembershipUpdate)
